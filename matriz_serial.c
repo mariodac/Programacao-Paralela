a38ef71449fe52ff8341df42ce3be8a13f29c544
mariodac <mdac.mario@gmail.com>
2019-09-26 20:43:28 -0300

    Aula dia 26 de setembro

--- matriz_serial.c
+++ matriz_serial.c
@@ -2,25 +2,31 @@
 #include "stdlib.h"
 #include "omp.h"
 
-#define TAM 99999
+#define TAM 1000
 long int matriz[TAM][TAM];
+long int matriz_soma[TAM][TAM];
 
 void operacaoMatriz(long int[][TAM], int);
-void salvarArquivo(char*);
+void matrizSoma(long int[][TAM], int);
+void salvarArquivo(char*, long int[][TAM]);
 
 int main(){
     double start, end;
     int tamanho = TAM;
 
     for(int i = 0; i < tamanho; i++)
-        for (int j = 0; j < tamanho; j++)
+        for (int j = 0; j < tamanho; j++){
             matriz[i][j] = 0;
+            matriz_soma[i][j] = 0;
+        }
 
     start = omp_get_wtime();
     operacaoMatriz(matriz, tamanho);
+    matrizSoma(matriz_soma, tamanho);
     end = omp_get_wtime();
     printf("Tempo: %lf segundos\n", end - start);
-    salvarArquivo("matrizSerial.txt");
+    salvarArquivo("matrizSerial.txt", matriz);
+    salvarArquivo("matrizSerialSoma.txt", matriz_soma);
     return 0;
 }
 
@@ -30,13 +36,78 @@ void operacaoMatriz(long int m[][TAM], int t){
             m[i][j] = (i + j) - (i * j);
 }
 
-void salvarArquivo(char *nome){
+void matrizSoma(long int m_soma[][TAM], int t){
+    int sup, inf, esq, dir;
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            if(i - 1 < 0 && j - 1 < 0){
+                dir = j + 1;
+                inf = i + 1;
+                esq = 0;
+                sup = 0;
+            }
+            else if(i + 1 == TAM && j + 1 == TAM){
+                esq = j - 1;
+                sup = i - 1;
+                dir = TAM - 1;
+                inf = TAM - 1;
+            }
+            else if(i + 1 == TAM && j - 1 < 0){
+                sup = i - 1;
+                dir = j + 1;
+                inf = TAM - 1;
+                esq = 0;
+            }
+            else if(i - 1 < 0 && j + 1 == TAM){
+                inf = i + 1;
+                esq = j - 1;
+                sup = 0;
+                dir = TAM - 1;
+            }
+            else if(j - 1 < 0){
+                sup = i - 1;
+                inf = i + 1;
+                esq = 0;
+                dir = j + 1;
+            }
+            else if(i - 1 < 0){
+                sup = 0;
+                inf = i + 1;
+                esq = j - 1;
+                dir = j + 1; 
+            }
+            else if(i + 1 == TAM){
+                sup = i - 1;
+                inf = TAM - 1;
+                esq = j - 1;
+                dir = j + 1;
+            }
+            else if(j + 1 == TAM){
+                sup = i - 1;
+                inf = i + 1;
+                esq = j - 1;
+                dir = TAM - 1;
+            }
+            else{
+                sup = i - 1;
+                inf = i + 1;
+                esq = j - 1;
+                dir = j + 1;
+            }
+            matriz_soma[i][j] = matriz[inf][j] + matriz[sup][j] + matriz[i][esq] + matriz[i][dir];
+        }
+    }
+}
+
+void salvarArquivo(char *nome, long int m[][TAM]){
     FILE *pont_arq;
     pont_arq = fopen(nome, "w");
     for (int i = 0; i < TAM; i++)
     {
         for (int j = 0; j < TAM; j++)
-            fprintf(pont_arq, "%ld\t", matriz[i][j]);
+            fprintf(pont_arq, "%ld\t", m[i][j]);
         fprintf(pont_arq, "\n");
         
     }
--- matriz_thread.c
+++ matriz_thread.c
@@ -2,11 +2,12 @@
 #include "stdlib.h"
 #include "pthread.h"
 #include "omp.h"
-#define TAM 10000
+#define TAM 1000
 #define N_THREADS 4
 #define N_ARGS 4
 
 long int mat[TAM][TAM];
+long int mat_soma[TAM][TAM];
 
 typedef struct {
     int inicio;
@@ -14,14 +15,17 @@ typedef struct {
 }ARG;
 
 void *operacaoMatriz(void*);
-void salvarArquivo(char*);
+void salvarArquivo(char*, long int[][TAM]);
+void *matrizSoma(void*);
 
 int main(){
     double start, end;
 
     for (int i = 0; i < TAM; i++)
-        for (int j = 0; j < TAM; j++)
+        for (int j = 0; j < TAM; j++){
             mat[i][j] = 0;
+            mat_soma[i][j] = 0;
+        }
     /* threads e argumentos sem uso de vetores */
     // pthread_t t1, t2;
     // struct args_struct arg1, arg2;
@@ -40,13 +44,41 @@ int main(){
     ARG args[N_ARGS];
     for (int i = 0; i < N_ARGS; i++)
     {
-        if(i == 0){
-            args[i].inicio = 0;
-            args[i].fim = TAM / N_ARGS;
+        if(args[i-1].fim == TAM) break;
+        if(TAM % 2 != 0){
+            
+            if(i == 0){
+                args[i].inicio = 0;
+                args[i].fim = TAM+1 / N_ARGS;
+            }
+            else{
+                args[i].inicio = args[i-1].fim;
+                args[i].fim = args[i].inicio + (TAM+1/N_ARGS);
+            }
+        }
+        else if(TAM % N_ARGS != 0){
+            if(i == 0){
+                args[i].inicio = 0;
+                args[i].fim = (TAM / N_ARGS) + 1;
+            }
+            else if(i == N_ARGS - 1){
+                args[i].inicio = args[i-1].fim;
+                args[i].fim = args[i].inicio + ((TAM / N_ARGS) + 2);
+            }
+            else{
+                args[i].inicio = args[i-1].fim;
+                args[i].fim = args[i].inicio + ((TAM / N_ARGS) + 1);
+            }
         }
         else{
-            args[i].inicio = args[i-1].fim;
-            args[i].fim = args[i].inicio + (TAM/N_ARGS);
+            if(i == 0){
+                args[i].inicio = 0;
+                args[i].fim = TAM / N_ARGS;
+            }
+            else{
+                args[i].inicio = args[i-1].fim;
+                args[i].fim = args[i].inicio + (TAM/N_ARGS);
+            }
         }
     }
     int c = 0;
@@ -62,34 +94,111 @@ int main(){
     }
     for (int i = 0; i < N_THREADS; i++)
         pthread_join(threads[i], NULL);
+    c = 0;
+    while (c != N_ARGS)
+    {
+        for (int i = 0; i < N_THREADS; i++)
+        {
+            pthread_create(&threads[i], NULL, matrizSoma, &args[c]);
+            c++;
+        }
+    }
+    for (int i = 0; i < N_THREADS; i++)
+        pthread_join(threads[i], NULL);
+    
     end = omp_get_wtime();
     printf("Tempo: %lf segundos\n", end - start);
     /* usando vetor para threads e argumentos */
     
-    salvarArquivo("matrizThread.txt");
+    salvarArquivo("matrizThread.txt", mat);
+    salvarArquivo("matrizThreadSoma.txt", mat_soma);
 
     pthread_exit(NULL);
+}
+
+void *matrizSoma(void *arguments){
+    ARG *args = arguments;
+    int sup, inf, esq, dir;
+    for (int i = args->inicio; i < args->fim; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            if(i - 1 < 0 && j - 1 < 0){
+                dir = j + 1;
+                inf = i + 1;
+                esq = 0;
+                sup = 0;
+            }
+            else if(i + 1 == TAM && j + 1 == TAM){
+                esq = j - 1;
+                sup = i - 1;
+                dir = TAM - 1;
+                inf = TAM - 1;
+            }
+            else if(i + 1 == TAM && j - 1 < 0){
+                sup = i - 1;
+                dir = j + 1;
+                inf = TAM - 1;
+                esq = 0;
+            }
+            else if(i - 1 < 0 && j + 1 == TAM){
+                inf = i + 1;
+                esq = j - 1;
+                sup = 0;
+                dir = TAM - 1;
+            }
+            else if(j - 1 < 0){
+                sup = i - 1;
+                inf = i + 1;
+                esq = 0;
+                dir = j + 1;
+            }
+            else if(i - 1 < 0){
+                sup = 0;
+                inf = i + 1;
+                esq = j - 1;
+                dir = j + 1; 
+            }
+            else if(i + 1 == TAM){
+                sup = i - 1;
+                inf = TAM - 1;
+                esq = j - 1;
+                dir = j + 1;
+            }
+            else if(j + 1 == TAM){
+                sup = i - 1;
+                inf = i + 1;
+                esq = j - 1;
+                dir = TAM - 1;
+            }
+            else{
+                sup = i - 1;
+                inf = i + 1;
+                esq = j - 1;
+                dir = j + 1;
+            }
+            mat_soma[i][j] = mat[inf][j] + mat[sup][j] + mat[i][esq] + mat[i][dir];
+        }
+    }
 }
 
 void *operacaoMatriz(void *arguments){
     ARG *args = arguments;
-    printf("Inicio: %d Fim: %d\n", args->inicio, args->fim);
+    // printf("Inicio: %d Fim: %d\n", args->inicio, args->fim);
     for(int i = args->inicio; i < args->fim; i++){
         for (int j = 0; j < TAM; j++)
-        {
             mat[i][j] = (i + j) - (i * j);
-        }
     }
     pthread_exit(NULL);
 }
 
-void salvarArquivo(char *nome){
+void salvarArquivo(char *nome, long int m[][TAM]){
     FILE *pont_arq;
     pont_arq = fopen(nome, "w");
     for (int i = 0; i < TAM; i++)
     {
         for (int j = 0; j < TAM; j++)
-            fprintf(pont_arq, "%ld\t", mat[i][j]);
+            fprintf(pont_arq, "%ld\t", m[i][j]);
         fprintf(pont_arq, "\n");
         
     }
