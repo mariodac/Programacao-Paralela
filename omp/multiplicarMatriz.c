5f113f11fbf0cf20062abf4e22917e7f42116be8
mariodac <mdac.mario@gmail.com>
2019-11-01 20:24:53 -0300

    utilizando alocação dinamica

---
+++ omp/multiplicarMatriz.c
@@ -0,0 +1,83 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "omp.h"
+#define N (int)1e3
+
+void multiplicar(int**, int**, int**);
+void gerarMatriz(int**, int**, int**);
+void imprimir(int**);
+void salvarArquivo(char*, int**);
+
+int main(){
+    double inicio, fim;
+    int **matriz1;
+    int **matriz2;
+    int **resultado;
+    matriz1 = (int**)malloc(sizeof(int*) * N);
+    matriz2 = (int**)malloc(sizeof(int*) * N);
+    resultado = (int**)malloc(sizeof(int*) * N);
+    for (int i = 0; i < N; i++)
+    {
+        matriz1[i] = (int*)malloc(sizeof(int) * N);
+        matriz2[i] = (int*)malloc(sizeof(int) * N);
+        resultado[i] = (int*)malloc(sizeof(int) * N);
+    }
+    
+    gerarMatriz(matriz1, matriz2, resultado);
+    inicio = omp_get_wtime();
+    multiplicar(matriz1, matriz2, resultado);
+    fim = omp_get_wtime();
+    printf("Tempo: %lf\n", fim-inicio);
+    // imprimir(resultado);
+    salvarArquivo("matrizCSerial.txt", resultado);
+    for (int i = 0; i < N; i++)
+    {
+        free(matriz1[i]);
+        free(matriz2[i]);
+        free(resultado[i]);
+    }
+    free(matriz1);
+    free(matriz2);
+    free(resultado);
+    return 0;
+}
+
+void gerarMatriz(int **mat1, int **mat2, int **resultado){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            mat1[i][j] = 1;
+            mat2[i][j] = 1;
+            resultado[i][j] = 0; 
+        }
+    }
+}
+
+void multiplicar(int **mat1, int **mat2, int **resultado) { 
+    for(int i = 0; i < N; i++) { 
+        for(int j = 0; j < N; j++){ 
+            for(int k = 0; k < N; k++) 
+                resultado[i][j] += (mat1[i][k])*(mat2[k][j]); 
+        } 
+    } 
+} 
+
+void imprimir(int **res){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++)
+            printf("%d \t", res[i][j]);
+        printf("\n");
+    }
+}
+
+void salvarArquivo(char *nome, int **m){
+    FILE *pont_arq;
+    pont_arq = fopen(nome, "w"); //abertura/criação do arquivo 
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            fprintf(pont_arq, "%d\t", m[i][j]); //escrita no arquivo
+        fprintf(pont_arq, "\n");
+        
+    }
+    fclose(pont_arq); //fechamento do arquivo
+}
--- omp/multiplicar_matriz.c
+++ omp/multiplicar_matriz.c
@@ -2,59 +2,45 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define TAM (int)1e4
+#define TAM (int)1e3
 
 void multiplicarDuasMatrizes(int**, int**, int**);
+void gerarMatriz(int**, int**, int**);
 void imprimir(int**);
+void salvarArquivo(char*, int**);
+
 int main()
 {
-    int **matrizA, **matrizB, **matrizC;
     double inicio, fim;
-    matrizA = (int **)malloc(sizeof(int *) * TAM); //alocando  um espaço onde a primeira  dimensão aponta para  outra dimensão
-    matrizB = (int **)malloc(sizeof(int *) * TAM); //alocando  um espaço onde a primeira  dimensão aponta para  outra dimensão
-    matrizC = (int **)malloc(sizeof(int *) * TAM); //alocando  um espaço onde a primeira  dimensão aponta para  outra dimensão
+    int **matrizA, **matrizB, **matrizC;
+    matrizA = (int**)malloc(sizeof(int*) * TAM);
+    matrizB = (int**)malloc(sizeof(int*) * TAM);
+    matrizC = (int**)malloc(sizeof(int*) * TAM);
     for (int i = 0; i < TAM; i++)
     {
-        matrizA[i] = (int *)malloc(sizeof(int) * TAM); //alocando  um espaço para segunda dimensão
-        matrizB[i] = (int *)malloc(sizeof(int) * TAM); //alocando  um espaço para segunda dimensão
-        matrizC[i] = (int *)malloc(sizeof(int) * TAM); //alocando  um espaço para segunda dimensão
+        matrizA[i] = (int*)malloc(sizeof(int) * TAM);
+        matrizB[i] = (int*)malloc(sizeof(int) * TAM);
+        matrizC[i] = (int*)malloc(sizeof(int) * TAM);
     }
-    for (int i = 0; i < TAM; i++)
-    {
-        for (int j = 0; j < TAM; j++)
-        {
-            matrizA[i][j] = 1;
-            matrizB[i][j] = 1;
-            matrizC[i][j] = 0;
-        }
-    }
+    gerarMatriz(matrizA, matrizB, matrizC);
     inicio = omp_get_wtime();
     multiplicarDuasMatrizes(matrizA, matrizB, matrizC);
     fim = omp_get_wtime();
     printf("Tempo: %lf\n", fim-inicio);
     // imprimir(matrizC);
-    for(int i = TAM; i >= 0; i--){
-        free(matrizA[i]);
-        free(matrizB[i]);
-        free(matrizC[i]);
-    }
-    free(matrizA);
-    free(matrizB);
-    free(matrizC);
+    salvarArquivo("matrizCSerial.txt", matrizC);
     return 0;
 }
 
-void multiplicarDuasMatrizes(int **matrizA, int **matrizB, int **matrizC){
+void multiplicarDuasMatrizes(int **matrizB, int **matrizA, int **matrizC){
     int i, j, k;
-    // #pragma omp parallel for default(none) private(i, j, k) shared(matrizA, matrizB, matrizC) collapse(3) schedule(static) num_threads(2)
+    #pragma omp parallel for default(none) private(i, j, k) shared(matrizA, matrizB, matrizC) collapse(3) schedule(static) num_threads(2)
     for (i = 0; i < TAM; i++)
     {
         for (j = 0; j < TAM; j++)
         {
             for (k = 0; k < TAM; k++)
             {
-                // if(k == 0)
-                    // printf("Total threads: %d\n", omp_get_num_threads());
                 matrizC[i][j] += matrizA[i][k] * matrizB[k][j];
             }
             
@@ -69,5 +55,29 @@ void imprimir(int **matriz){
         for(int j = 0; j < TAM; j++)
             printf("%d \t", matriz[i][j]);
         printf("\n");
+    }
+}
+
+void salvarArquivo(char *nome, int **m){
+    FILE *pont_arq;
+    pont_arq = fopen(nome, "w"); //abertura/criação do arquivo 
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+            fprintf(pont_arq, "%d\t", m[i][j]); //escrita no arquivo
+        fprintf(pont_arq, "\n");
+        
+    }
+    fclose(pont_arq); //fechamento do arquivo
+}
+void gerarMatriz(int **matrizA, int **matrizB, int **matrizC){
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            matrizA[i][j] = 1;
+            matrizB[i][j] = 1;
+            matrizC[i][j] = 0;
+        }
     }
 }
